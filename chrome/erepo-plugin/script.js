3b43c05c95f59c2132f5f6551941fcb2a738ee0d
Masahiro Yamamoto <m-yamamt@ist.osaka-u.ac.jp>
2017-11-20 12:26:43 +0900

    share setting between options page and content script

--- chrome/erepo-plugin/script.js
+++ chrome/erepo-plugin/script.js
@@ -1,4 +1,8 @@
 console.log('Error Repository Plugin starts monitoring');
+chrome.storage.sync.get(['query', 'cookie', 'verify'], function(data) {
+    console.log(data);
+});
+console.log(document.cookie);
 
 // URLのクエリ部分を隠すfunction
 var hideQuery = function(str) {
